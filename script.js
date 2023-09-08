249e946b581cc4c63859a4b869690c908aace4d2
Justine Dakiss <justinedaquis28@gmail.com>
2023-09-08 10:42:09 +0800

    added styling

---
+++ script.js
@@ -0,0 +1,15 @@
+// create css classes for each part of layout when dashboard is shown or hidden
+// in each class, grid area is specified 
+// create css class for main container when db is shown or hidden as well
+//  specify grid template area for when shown or hidden
+
+//  when db is shown
+// change classlist of each part of layout
+// change main cont class list to change grid area temp
+
+
+// when user clicks on button:
+// dashboard shows up from the left
+// 
+//  -adding dashboard container to main cont
+//  -
